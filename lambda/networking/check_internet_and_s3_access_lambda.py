e249e0745a6bd7c9242473b677e5f884e7e6ded2
kankou-aliaksei <aliaksei.kankou@gmail.com>
2022-08-12 10:03:43 +0400

    Updated check_internet_and_s3_access_lambda.py

--- lambda/networking/check_internet_and_s3_access_lambda.py
+++ lambda/networking/check_internet_and_s3_access_lambda.py
@@ -1,18 +1,33 @@
 import json
+# arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-requests:6
 import requests
 import boto3
+from botocore.config import Config
 
-s3 = boto3.client('s3')
+REQUEST_TIMEOUT = 7
+BOTO3_CONNECT_TIMEOUT = 7
+
+config = Config(
+    retries={
+        'max_attempts': 1,
+        'mode': 'standard'
+    },
+    connect_timeout=BOTO3_CONNECT_TIMEOUT
+)
+
+s3 = boto3.client('s3', config=config)
 
 
 def lambda_handler(event, context):
     try:
-        receive = requests.get('https://ipinfo.io/ip')
-        print('Public internet available')
+        url = 'https://ipinfo.io/ip'
+
+        receive = requests.get(url, timeout=REQUEST_TIMEOUT)
+        print('URL available')
         print('Content:')
         print(receive.content)
     except Exception as e:
-        print('Public internet not available')
+        print('URL not available')
     try:
         response = s3.list_buckets()
         print('S3 access available')
